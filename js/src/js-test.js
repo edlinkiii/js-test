530b92fb1e66f806d9f76df431e771122e0e37b5
Ed Link III <linkiii@gmail.com>
2023-01-26 12:52:06 -0500

    Tidied up formatting

--- js/src/js-test.js
+++ js/src/js-test.js
@@ -14,7 +14,6 @@ export const JSTest = {
         const { description, collection, condition } = suite;
 
         if (!condition || condition()) {
-
             let count = 0;
             let passed = 0;
             let skipped = 0;
@@ -34,14 +33,13 @@ export const JSTest = {
                     const wait = await JSTest.wait(parseFloat(test.waitSec || 0) || 0);
 
                     const result = await JSTest.run(test.description, JSTest.shouldBe(execute(), expected, exactEqual), test.message, count);
-    
+
                     result && passed++;
                 } else {
                     skipped++;
 
-                        JSTest.infoLog(`${count}) SKIPPED: ${test.description} -- Condition not met`);
+                    JSTest.infoLog(`${count}) SKIPPED: ${test.description} -- Condition not met`);
                 }
-
             }, Promise.resolve());
 
             let failed = count - (passed + skipped);
@@ -61,45 +59,44 @@ export const JSTest = {
             const result = await JSTest.run(test.description, test.testIt, test.message);
 
             result && passed++;
-
         }, Promise.resolve());
 
         console.log(`%c End test batch "${description}" | %cPassed ${passed} of ${count} `, JSTest.logColor.info, passed === count ? JSTest.logColor.passed : JSTest.logColor.failed);
     },
 
-    test: async (description, testIt, message="") => {
+    test: async (description, testIt, message = "") => {
         const result = await JSTest.run(description, testIt, message);
 
         return result;
     },
 
-    run: async (description, testIt, message="", count=0) => {
+    run: async (description, testIt, message = "", count = 0) => {
         return await testIt.then((result) => {
-            if(result.passed) {
+            if (result.passed) {
                 console.log(`%c ${count > 0 ? count + ")" : ""} PASSED: ${description}${message.length > 0 ? " | " + message : ""} `, JSTest.logColor.passed, JSTest.testDebug ? result : "");
             } else {
                 console.log(`%c ${count > 0 ? count + ")" : ""} FAILED: ${description}${message.length > 0 ? " | " + message : ""} `, JSTest.logColor.failed, JSTest.testDebug ? result : "");
             }
-    
+
             return result.passed;
         });
     },
 
-    shouldBe: (actual, expected=true, strict=true) => {
+    shouldBe: (actual, expected = true, strict = true) => {
         return new Promise((res) => {
             let value1, value2, passed;
 
-            if(!strict && (expected === true || expected === "true" || expected === "truthy" || !expected)) {
+            if (!strict && (expected === true || expected === "true" || expected === "truthy" || !expected)) {
                 value1 = !!actual;
                 value2 = true;
-            } else if(!strict && (expected === false || expected === "false" || expected === "falsy")) {
+            } else if (!strict && (expected === false || expected === "false" || expected === "falsy")) {
                 value1 = !actual;
                 value2 = false;
             } else {
                 value1 = actual;
                 value2 = expected;
             }
-    
+
             passed = strict ? value1 === value2 : value1 == value2;
 
             const ret = {
@@ -108,7 +105,7 @@ export const JSTest = {
                 strict,
                 passed,
             };
-    
+
             res(ret);
         });
     },
